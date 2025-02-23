5d52978e1951a61584dc094dbb630a1eb3b54350
unknown <drivemah.tah@gmail.com>
2025-02-23 16:04:07 +0200

    Console APP

---
+++ Student.cs
@@ -0,0 +1,7 @@
+// model of student entity
+public class Student
+{
+    private string Id { set; get; }
+    private string Name { set; get; }
+    private string Major { set; get; }
+}
---
+++ StudentDbContext.cs
@@ -0,0 +1,4 @@
+public class StudentDbContext : DbContext
+{
+    
+}
