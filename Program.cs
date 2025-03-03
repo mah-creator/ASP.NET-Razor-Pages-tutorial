b8bd153cd0f2a3de2e06eba85b5c5c508645d80b
mahmoud <drivemah.tah@gmail.com>
2025-03-03 08:17:18 +0200

    Execute two tasks asynchronously

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static async Task Main()
+    {
+        Task sleepTask = Sleep(); // Start Sleep() asynchronously without awaiting it yet
+        Console.WriteLine("Await delay over!"); // This will print immediately
+
+
+        await sleepTask; // Now wait for Sleep() to finish
+    }
+
+    static async Task Sleep()
+    {
+        Console.WriteLine("Delay begins.");
+        await Task.Delay(3000);
+        Console.WriteLine("Delay over.");
+    }
+}
